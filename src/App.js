a134cca3b56ee30057b7f162a1cc0d2a3961b604
Gardern-eggs <104757097+Gardern-eggs@users.noreply.github.com>
2022-10-17 01:58:56 +0100

    added header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Countries from './componets/countries';
+import Header from './componets/header';
+
 
 // to render countries components
 function App() {
   return (
-    <> 
+    <>
+      <Header /> 
       <Countries />
+      
     </>
   );
 }
