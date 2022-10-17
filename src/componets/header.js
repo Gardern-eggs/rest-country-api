5cb00ff5c423951c6fb5d10503f307d31476b258
Gardern-eggs <104757097+Gardern-eggs@users.noreply.github.com>
2022-10-17 01:59:29 +0100

    build the header component

---
+++ src/componets/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="headerContainer">
+        <div id='question'> 
+            <h3>Where in the world?</h3>
+        </div>
+        <div id='modeSwitch'>
+        <i className="fa-solid fa-moon-stars"></i>
+           <h6>Dark Mode</h6> 
+        </div>
+    </header>
+  )
+}
+
+export default Header
