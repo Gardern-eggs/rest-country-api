1f2af21a4e77d7481c3ab967dfa7e5187f361dda
Gardern-eggs <104757097+Gardern-eggs@users.noreply.github.com>
2022-10-22 08:39:22 +0100

    component to hold individual country

---
+++ src/componets/country.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Country = () => {
+  return (
+    <>
+        <link typeof='/' className='btn btn-fade'>
+        <i className=''></i>Back Home    
+         </link>
+    <h1>country</h1>
+    </>
+  )
+}
+export default Country
