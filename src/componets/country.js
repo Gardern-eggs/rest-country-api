de7df7074270a8017e8f03611eebfcd406207867
Gardern-eggs <104757097+Gardern-eggs@users.noreply.github.com>
2022-10-23 18:54:26 +0100

    resturcturing fetched data fo easy reference

--- src/componets/country.js
+++ src/componets/country.js
@@ -13,7 +13,7 @@ const Country = () => {
     const response = await fetch( `${url}${name}`)
     const country = await response.json()
       setCountry(country)
-      console.log(country)
+    
     
   }
   useEffect(()=>{ 
@@ -24,7 +24,48 @@ const Country = () => {
     <div className='btn-back'>
       <Link to='/' className='btn'><i className='fa fa-arrow-left' > Back</i></Link>
     </div>
-      <h1>country</h1>
+      <div className='country'>
+        {country.map((c)=>{
+          
+          const {numericCode, flag, name, nativeName, population, region, subregion, capital, topLevelDomain, currencies, languages, borders}=c
+
+          return(
+            <article key={numericCode}>
+              <div className='selectcountryBox'>
+                <div className='imgContainer'>
+                  <img src={flag}/>
+                </div>     
+                <div className='countryBoxDetails'>
+                 
+                  <div className='mainDetails'>
+                  <h3> {name}</h3>
+                      <h4>Native name: <span>{nativeName}</span></h4>
+                      <h4>Population: <span>{population}</span></h4>
+                      <h4>Region: <span>{region}</span></h4>
+                      <h4>Sub Region: <span>{subregion}</span></h4>
+                      <h4>Capital: <span>{capital}</span></h4>
+                  </div>
+                 <div className='otherDetails'>
+                    <h4>Top Level Domain: <span>{topLevelDomain}</span></h4>
+{/* since the currency and language were objects the .name property accesses the name of the variable */}
+                      <h4>Currencies: <span>{currencies[0].name}</span></h4>
+                      <h4>Languages: <span>{languages[0].name}</span></h4>
+                  </div>
+                  <div className='borders'>
+                    <h4>Border Countries: <span>{borders}</span></h4> 
+                  </div>
+                  
+                      
+                  
+                  </div>         
+               </div>
+            </article>
+
+
+          )
+
+        })}
+      </div>
     </>
   )
 }
