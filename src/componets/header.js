e21e8b1975632e1fe47258f2dc377faa0b411635
Gardern-eggs <104757097+Gardern-eggs@users.noreply.github.com>
2022-11-15 13:05:04 +0000

    darkmode build on header comp

--- src/componets/header.js
+++ src/componets/header.js
@@ -1,16 +1,43 @@
-import React from 'react'
-import DarkModeIcon from '@mui/icons-material/DarkMode'
+import React, { useState } from 'react'
+import DarkMode from '@mui/icons-material/DarkMode'
 
-const Header = () => {
+
+const Header = () => { 
+  const [mode,setMode]=useState("light")
+  const switchMode=()=>{
+    if (mode === 'light') {
+      setMode('dark');
+      document.documentElement.style.setProperty('--primary', 'hsl(207, 26%, 17%)');
+      document.documentElement.style.setProperty('--secondary', ' hsl(209, 23%, 22%)');
+      document.documentElement.style.setProperty('--primary-text', 'hsl(0, 0%, 100%)');
+
+  }
+  else {
+      document.documentElement.style.setProperty('--primary', 'hsl(0, 0%, 100%)');
+      document.documentElement.style.setProperty('--secondary', ' hsl(0, 0%, 98%)');
+      document.documentElement.style.setProperty('--primary-text', 'hsl(200, 15%, 8%)');
+      setMode('light');
+  }
+}
+  
+
   return (
-    <header className="headerContainer">
+    <header className="headerContainer light-mode ">
         <div id='question'> 
             <h2>Where in the world?</h2>
         </div>
-        <div id='modeSwitch'>
-        <DarkModeIcon className="moon-o"/>
-       
-           <h4>Dark Mode</h4>  
+        <div id='modeSwitch'onClick={switchMode}>
+        <DarkMode  />
+        {mode === 'dark' ? 
+                <>
+                    <span>Light Mode</span>
+                </>
+                :
+                    <>   
+                      <span >Dark Mode</span>
+                    </>
+                }
+           
         </div>
     </header>
   )
