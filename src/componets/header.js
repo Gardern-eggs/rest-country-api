2976832350ff343664e3da437452695290136442
Gardern-eggs <104757097+Gardern-eggs@users.noreply.github.com>
2022-11-18 13:42:03 +0000

    adding a new variable to hold element data toggle

--- src/componets/header.js
+++ src/componets/header.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import DarkMode from '@mui/icons-material/DarkMode'
+import { rgbToHex } from '@mui/material'
 
 const Header = () => { 
   const [mode,setMode]=useState("light")
@@ -9,21 +10,23 @@ const Header = () => {
       document.documentElement.style.setProperty('--primary', 'hsl(207, 26%, 17%)');
       document.documentElement.style.setProperty('--secondary', ' hsl(209, 23%, 22%)');
       document.documentElement.style.setProperty('--primary-text', 'hsl(0, 0%, 100%)');
+      document.documentElement.style.setProperty('--border'," rgb(18, 28, 37)");
 
   }
   else {
       document.documentElement.style.setProperty('--primary', 'hsl(0, 0%, 100%)');
       document.documentElement.style.setProperty('--secondary', ' hsl(0, 0%, 98%)');
       document.documentElement.style.setProperty('--primary-text', 'hsl(200, 15%, 8%)');
+      document.documentElement.style.setProperty('--border'," rgb(198, 204, 206)");
+
       setMode('light');
   }
 }
   
 
   return (
-    <header className="headerContainer light-mode ">
+    <header className="headerContainer">
         <div id='question'> 
-        
             <h2>Where in the world?</h2>
         </div>
         <div id='modeSwitch'onClick={switchMode}>
