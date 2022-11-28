142d65787938b5ae2e1fb43c5d070b9b2289dd56
Gardern-eggs <104757097+Gardern-eggs@users.noreply.github.com>
2022-11-28 15:13:48 +0000

    import the axios

--- src/App.js
+++ src/App.js
@@ -3,10 +3,26 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Countries from './componets/countries';
 import Country from './componets/country';
 import Header from './componets/header';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
+
+const url = 'https://restcountries.com/v2/all';
 
 // to render countries components
 function App() {
+
+  const [countries,setCountries]=useState([])
+  useEffect(()=>{ 
+   
+    axios.get(url).then((response)=>{
+        const countries = response.data
+        setCountries(countries)
+ 
+    }).catch((err)=>console.log(err))
+
+ 
+  }, []) 
   return (
     <Router>
       <>
@@ -14,10 +30,10 @@ function App() {
 
         <Route exact path='/'>
         
-          <Countries />
+          <Countries arrayData={countries}/>
         </Route>
         <Route path='/countries/:name'>
-          <Country />
+          <Country arrayData={countries} />
         </Route> 
         </>
     </Router>
