2b623e7dea62c224160b29a984646c36e0d8bb91
Gardern-eggs <104757097+Gardern-eggs@users.noreply.github.com>
2022-11-30 12:19:07 +0000

    making filter components work

--- src/componets/countries.js
+++ src/componets/countries.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 
@@ -7,8 +8,8 @@ const Countries = (props) => {
     
     // retreving data from apps.js
     let countries=props.arrayData;
-
-
+    const [search,setSearch]=useState('')
+    const [region,setRegion]=useState('')
     
   return (
     <>
@@ -16,12 +17,12 @@ const Countries = (props) => {
     <div className='filterbar'>
         <form className='formControl'>
       <div className='search'>
-        <i className="fa fa-search"> <input type="search" id="search" placeholder='Search for a country…'  /></i>
+        <i className="fa fa-search"> <input type="search" id="search" placeholder='Search for a country…'  onChange={(e)=>{setSearch(e.target.value)}}/></i>
         </div>
         </form>
         
         <div className='option'>
-            <select name='select' id='select' >
+            <select name='select' id='select' onChange={(e)=>{setRegion(e.target.value)}}>
             <option value=""defaultValue >Filter by Region </option>
                 <option value='Africa'>Africa</option>
                 <option value='Americas'>Americas</option>
@@ -33,7 +34,26 @@ const Countries = (props) => {
     </div>
     {/* end of filter component */}
         <div className='display'>
-            {countries.map((country)=>{
+            {/* filter component to search for countries */}
+            {countries.filter((val)=>{
+            if(search === ''){
+               return val
+            }else if(val.name.toLowerCase().includes(search.toLocaleLowerCase())){
+              return val
+            }
+                }) 
+        // filter to select regions
+        .filter((val)=>{
+            if(region === '' ){
+ 
+              return val
+
+           }else if(val.region.includes(region)){
+
+             return val
+
+           }
+        }).map((country)=>{
     //data requested to complete project
                 const { numericCode,flag, name, population, region, capital, }= country
 
