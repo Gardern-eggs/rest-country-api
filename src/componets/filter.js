ae0d5c951db0a2efb501aacb6323afb7ba47755c
Gardern-eggs <104757097+Gardern-eggs@users.noreply.github.com>
2022-11-30 14:44:18 +0000

    remove filter compnent

--- src/componets/filter.js
+++ src/componets/filter.js
@@ -1,58 +1,58 @@
-import React from 'react'
+// import React from 'react'
 
 
-export const Filter = ({setCountries,fetchCountryData}) => {
+// export const Filter = ({setCountries,fetchCountryData}) => {
    
-    const searchData=async(data)=>{
-      if (data!==''){
-        const response=await fetch(`https://restcountries.com/v2/name/${data}`)
-        const biibi=await response.json();
-        if(biibi.length>0){
-          await setCountries(biibi)
-          console.log(biibi)
-        }
-      }else{
-   fetchCountryData()
-      }
-    }
+//     const searchData=async(data)=>{
+//       if (data!==''){
+//         const response=await fetch(`https://restcountries.com/v2/name/${data}`)
+//         const biibi=await response.json();
+//         if(biibi.length>0){
+//           await setCountries(biibi)
+//           console.log(biibi)
+//         }
+//       }else{
+//    fetchCountryData()
+//       }
+//     }
 
-    const searchRegion=async(data)=>{
-      if (data!==''){
-        const response=await fetch(`https://restcountries.com/v2/region/${data}`)
-        const biibi=await response.json();
-        if(biibi.length>0){
-          await setCountries(biibi)
-     //     console.log(biibi)
-        }
-      }else{
-  fetchCountryData()
-      }
-    }
+//     const searchRegion=async(data)=>{
+//       if (data!==''){
+//         const response=await fetch(`https://restcountries.com/v2/region/${data}`)
+//         const biibi=await response.json();
+//         if(biibi.length>0){
+//           await setCountries(biibi)
+//      //     console.log(biibi)
+//         }
+//       }else{
+//   fetchCountryData()
+//       }
+//     }
   
   
 
 
 
-  return (
-    <div className='filterbar'>
-        <form className='formControl'>
-      <div className='search'>
-        <i className="fa fa-search"> <input type="search" id="search" placeholder='Search for a country…'  onChange={(e)=>searchData(e.target.value)}  /></i>
-        </div>
-        </form>
+//   return (
+//     <div className='filterbar'>
+//         <form className='formControl'>
+//       <div className='search'>
+//         <i className="fa fa-search"> <input type="search" id="search" placeholder='Search for a country…'  onChange={(e)=>searchData(e.target.value)}  /></i>
+//         </div>
+//         </form>
         
-        <div className='option'>
-            <select name='select' id='select' onChange={(e)=>searchRegion(e.target.value)}>
-            <option value=""defaultValue >Filter by Region </option>
-                <option value='Africa'>Africa</option>
-                <option value='Americas'>Americas</option>
-                <option value='Asia'>Asia</option>
-                <option value='Europe'>Europe</option>
-                <option value='Oceania'>Oceania</option>
-            </select>
-        </div>
+//         <div className='option'>
+//             <select name='select' id='select' onChange={(e)=>searchRegion(e.target.value)}>
+//             <option value=""defaultValue >Filter by Region </option>
+//                 <option value='Africa'>Africa</option>
+//                 <option value='Americas'>Americas</option>
+//                 <option value='Asia'>Asia</option>
+//                 <option value='Europe'>Europe</option>
+//                 <option value='Oceania'>Oceania</option>
+//             </select>
+//         </div>
         
-    </div>
+//     </div>
     
-  )
-}
+//   )
+// }
